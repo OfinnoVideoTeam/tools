73f57e5f1280e4517dd905dc402f1c7acd67c385
tae-mpeg <78283907+tae-mpeg@users.noreply.github.com>
2021-03-02 17:52:31 -0500

    Update setup.s3fs.sh

--- setup.s3fs.sh
+++ setup.s3fs.sh
@@ -1,6 +1,7 @@
 # install s3fs on amazon linux
 sudo yum update
-sudo yum install automake fuse fuse-devel gcc-c++ git libcurl-devel libxml2-devel make openssl-develgit clone https://github.com/s3fs-fuse/s3fs-fuse.git
+sudo yum install automake fuse fuse-devel gcc-c++ git libcurl-devel libxml2-devel make openssl-devel
+git clone https://github.com/s3fs-fuse/s3fs-fuse.git
 
 cd  s3fs-fuse
 ./autogen.sh
